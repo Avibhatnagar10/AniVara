7395df502e49dd565cd7d7a7842fea41c17dfc57
Avi Bhatnagar <91671078+Avibhatnagar10@users.noreply.github.com>
2025-03-07 12:41:47 +0530

    Update page.tsx

--- anime-site/src/app/page.tsx
+++ anime-site/src/app/page.tsx
@@ -18,14 +18,11 @@ import Footer from "./Components/Footer";
 import AnimeCarousel from "./Components/AnimeCarousel";
 
 export default function Home() {
-  const [selectedCategory, setSelectedCategory] = useState("All");
   const [loading, setLoading] = useState(true); // Loading state
 
   useEffect(() => {
     setTimeout(() => setLoading(false), 2000); // Simulate loading time
   }, []);
-
-  const categories = ["All", "Action", "Romance", "Comedy", "Adventure", "Fantasy", "Sports", "Sci-Fi", "Thriller", "Horror"];
 
   const topAnime = [
     { title: "Naruto", category: "Action", img: "/naruto.jpeg" },
@@ -36,27 +33,6 @@ export default function Home() {
     { title: "Weathering with You", category: "Romance", img: "/weathering.jpeg" },
     { title: "Haikyuu", category: "Sports", img: "/haikyuu.png" },
   ];
-
-  const animeList = [
-    { title: "One Punch Man", category: "Comedy", img: "/punch.jpeg" },
-    { title: "Haikyuu", category: "Sports", img: "/haikyuu.png" },
-    { title: "Horimiya", category: "Romance", img: "/hori1.jpeg" },
-    { title: "One Piece", category: "Adventure", img: "/luffy1.jpeg" },
-    { title: "My Dress Up Darling", category: "Fantasy", img: "/Kitagawa1.jpeg" },
-    { title: "Spy X Family", category: "Action", img: "/spy.jpeg" },
-    { title: "Your Name", category: "Romance", img: "/yourname.jpeg" },
-    { title: "Solo Leveling", category: "Adventure", img: "/solo1.jpeg" },
-    { title: "Suzume", category: "Adventure", img: "/suzume.jpeg" },
-    { title: "Jujutsu Kaisen", category: "Adventure", img: "/jjk.jpg" },
-    { title: "Tokyo Revengers", category: "Action", img: "/mikey.jpeg" },
-    { title: "Terminator Zero", category: "Sci-Fi", img: "/Terminator.jpeg" },
-    { title: "Re:Zero", category: "Thriller", img: "/rezero.jpeg" },
-    { title: "Parasyte", category: "Horror", img: "/Parasyte.jpeg" },
-    { title: "The Angel Next Door", category: "Romance", img: "/Shiina Mahiru.jpeg" },
-    { title: "I Want to Eat Your Pancreas", category: "Romance", img: "/eat.jpeg" },
-  ];
-
-  const filteredAnime = selectedCategory === "All" ? animeList : animeList.filter((anime) => anime.category === selectedCategory);
 
   return (
     <Box sx={{ minHeight: "100vh", bgcolor: "black" }}>
